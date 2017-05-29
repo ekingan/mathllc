6687a9ccd416b4ceec1f7abac89886b6602ea11c
ekingan <ekingan@gmail.com>
2017-05-29 12:30:42 -0700

    Changed filter to select

--- app/admin/payment.rb
+++ app/admin/payment.rb
@@ -1,5 +1,9 @@
 ActiveAdmin.register Payment do
 permit_params :amount, :payment_type, :check_number
 
+filter :amount
+filter :payment_type, as: :select
+filter :check_number
+filter :created_at
 
 end
