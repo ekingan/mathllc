38b85fb3c4372afe71e13f6719245688e1af356a
ekingan <ekingan@gmail.com>
2017-05-29 12:26:28 -0700

    payment model

---
+++ app/admin/payment.rb
@@ -0,0 +1,5 @@
+ActiveAdmin.register Payment do
+permit_params :amount, :payment_type, :check_number
+
+
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170529182304) do
+ActiveRecord::Schema.define(version: 20170529192214) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -70,6 +70,14 @@ ActiveRecord::Schema.define(version: 20170529182304) do
     t.text     "notes"
     t.datetime "created_at",           null: false
     t.datetime "updated_at",           null: false
+  end
+
+  create_table "payments", force: :cascade do |t|
+    t.decimal  "amount"
+    t.integer  "payment_type"
+    t.integer  "check_number"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
   end
 
   create_table "preparers", force: :cascade do |t|
